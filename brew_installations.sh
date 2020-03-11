84eba4bb752040ea7267b53be51ba44c9375cb43
Daniil K <38178976+kistanod@users.noreply.github.com>
2020-03-11 00:33:07 -0700

    added some automated changes

---
+++ brew_installations.sh
@@ -0,0 +1,29 @@
+" install the Homebrew
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+" Fetch latest version of homebrew and formula
+
+brew update
+
+" install all the necessary packages
+
+brew install cask
+brew install htop
+brew install speedtest-cli
+brew install youtube-dl
+brew install pyenv
+brew install neovim
+brew install trash
+brew install fish
+
+" install apps that i use the most
+
+brew cask install discord
+brew cask install iTerm2
+brew cask install slack 
+brew cask install visual-studio-code
+brew cask install firefox
+
+NOT DONE YET
+
