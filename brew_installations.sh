e2816d2048c7e6b11caba9dd1adfd1efde045270
Daniil K <38178976+kistanod@users.noreply.github.com>
2020-03-11 14:03:30 -0700

    yep

--- brew_installations.sh
+++ brew_installations.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 " install the Homebrew
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
@@ -6,24 +8,39 @@
 
 brew update
 
-" install all the necessary packages
+# install all the necessary packages
 
 brew install cask
-brew install htop
-brew install speedtest-cli
-brew install youtube-dl
-brew install pyenv
-brew install neovim
-brew install trash
-brew install fish
+brew install htop # top with extras
+brew install speedtest-cli # so i don't have to google
+brew install youtube-dl # download youtube from commandline
+brew install pyenv # lovely python manager
+brew install neovim # yeeeeeee
+brew install trash # automatic trashing instead of rm
+brew install fish # my favourite shell
+brew install mas # cli-appstore
+brew install tmux # leave commands running in the background 
 
-" install apps that i use the most
+# all of the install apps that i can install from cask
 
 brew cask install discord
 brew cask install iTerm2
 brew cask install slack 
 brew cask install visual-studio-code
 brew cask install firefox
+brew cask install dropbox
+brew cask install 1password
+brew cask install appcleaner
+brew cask install skype
+brew cask install alfred
+brew cask install bartender
+brew cask install keka
+brew cask install zoom
+
+# all of the apps i can install from cask
+
+mas lucky things
+mas lucky twitter
 
 NOT DONE YET
 
